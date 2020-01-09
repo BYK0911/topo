e47e31001c2a67e4182a07e1e229914f2c94467f
baoyukun <bykmail1992@163.com>
2020-01-09 14:41:32 +0800

    元素拖拽插件开发

---
+++ lib/plugin/draggable.ts
@@ -0,0 +1,32 @@
+import TopoBlock from '../core/classes/block';
+import TopoEvent from '../event/TopoEvent';
+
+function draggable (el: TopoBlock ):void {
+  let prevX: number;
+  let prevY: number;
+
+  el.on('dragstart', function (e: TopoEvent) {
+    e.stopPropagation();
+    prevX = e.originalEvent.offsetX;
+    prevY = e.originalEvent.offsetY;
+  })
+  
+  el.on('drag', function (e: TopoEvent) {
+    e.stopPropagation();
+    let dx: number, dy: number;
+    if (el.parent) {
+      let { x: x0, y: y0 } = el.parent.getRelativeCoord(prevX, prevY);
+      let { x: x1, y: y1 } = el.parent.getRelativeCoord(e.originalEvent.offsetX, e.originalEvent.offsetY);
+      dx = x1 - x0;
+      dy = y1 - y0;
+    } else {
+      dx = e.originalEvent.offsetX - prevX;
+      dy = e.originalEvent.offsetY - prevY;
+    }
+    el.translate(dx, dy);
+    prevX = e.originalEvent.offsetX;
+    prevY = e.originalEvent.offsetY;
+  })
+}
+
+export default draggable;
