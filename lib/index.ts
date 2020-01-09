c7d0b6e425b856a1a3bf03c4f98b518df8198880
baoyukun <bykmail1992@163.com>
2020-01-09 13:38:52 +0800

    修复视图拖拽与缩放bug

--- lib/index.ts
+++ lib/index.ts
@@ -27,22 +27,28 @@ const topo = {
     })
 
     view.on('dragstart', e => {
-      e.stopPropergation();
-      prevX = e.originalEvent.offsetX;
-      prevY = e.originalEvent.offsetY;
-    })
-    view.on('drag', e => {
-      e.stopPropergation();
-      let { x: x0, y: y0 } = e.target.getRelativeCoord(prevX, prevY);
-      let { x: x1, y: y1 } = e.target.getRelativeCoord(e.originalEvent.offsetX, e.originalEvent.offsetY);
-      let dx = x1 - x0;
-      let dy = y1 - y0;
-      
-      e.target.translate(dx, dy);
+      e.stopPropagation();
       prevX = e.originalEvent.offsetX;
       prevY = e.originalEvent.offsetY;
     })
 
+    view.on('drag', e => {
+      e.stopPropagation();
+      let dx: number, dy: number;
+      if (e.target.parent) {
+        let { x: x0, y: y0 } = e.target.parent.getRelativeCoord(prevX, prevY);
+        let { x: x1, y: y1 } = e.target.parent.getRelativeCoord(e.originalEvent.offsetX, e.originalEvent.offsetY);
+        dx = x1 - x0;
+        dy = y1 - y0;
+      } else {
+        dx = e.originalEvent.offsetX - prevX;
+        dy = e.originalEvent.offsetY - prevY;
+      }
+      e.target.translate(dx, dy);
+      prevX = e.originalEvent.offsetX;
+      prevY = e.originalEvent.offsetY;
+    })
+  
     return view;
   }
 };
