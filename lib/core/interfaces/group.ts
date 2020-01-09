e5c7ef0591b09d11e7cd57a5d975b4c0d4f974e4
baoyukun <bykmail1992@163.com>
2020-01-09 18:18:01 +0800

    添加getter: chldren

--- lib/core/interfaces/group.ts
+++ lib/core/interfaces/group.ts
@@ -3,8 +3,9 @@ import TopoBlock from './block';
 import TopoElement from './element';
 
 interface TopoGroup extends TopoBlock{
-  elements: TopoElement[];
-
+  children: TopoElement[];
+  backgroundImage: string;
+  
   add: (element: TopoElement) => void;
   remove: (element: TopoElement) => void;
   clear: (element: TopoElement) => void;
