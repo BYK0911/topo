63083a3bd03b912d5c057e3c6bbc748a28f9c0ec
baoyukun <bykmail1992@163.com>
2020-01-10 11:40:48 +0800

    线碰撞检测bug修复

--- lib/util/isPointOnLine.ts
+++ lib/util/isPointOnLine.ts
@@ -2,15 +2,16 @@ import vector from './vector';
 
 function isPointOnLine (x1: number, y1: number, x2: number, y2: number, x: number, y: number): boolean {
   const r = 2; // 容差
-  let v1 = [x2 - x1, y2 - y1],
-    v2 = [x - x1, y - y1],
-    n1 = vector.norm(v1),
-    n2 = vector.norm(v2);
 
-    if (n2 <= r) return true;
-    if (n1 && vector.crossNorm(v1, v2) / n1 <= r) return true;
+  let v1 = [x2 - x1, y2 - y1], v = [x - x1, y - y1],
+    n1 = vector.norm(v1), n = vector.norm(v),
+    d = vector.dot(v1, v),
+    c = vector.crossNorm(v1, v);
 
-    return false; 
+  if (n <= r) return true;
+  if (d / n1 > -r && d / n1 <= n1 + r && c / n1 <= r) return true;
+
+  return false; 
 }
 
 export default isPointOnLine;
