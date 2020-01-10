b63e0bc6d70837bf373dd0f77e4cc0d76f863543
baoyukun <bykmail1992@163.com>
2020-01-10 11:41:40 +0800

    添加坐标解析方法

--- lib/core/classes/block.ts
+++ lib/core/classes/block.ts
@@ -56,7 +56,7 @@ abstract class TopoBlock extends TopoElement {
     this.scale *= isZoomIn ? 1.1 : 1 / 1.1;
   }
 
-  getRelativeCoord (x:number = 0, y:number = 0) {
+  getRelativeCoord (x:number = 0, y:number = 0):Coord {
     let coord = new Coord(x, y);
 
     coord.translate(-this.x, -this.y);
@@ -66,12 +66,44 @@ abstract class TopoBlock extends TopoElement {
     return coord;
   }
 
-  getAbsoluteCoord (x:number = 0, y:number = 0) {
+  getAbsoluteCoord (x:number = 0, y:number = 0): Coord {
     let coord = new Coord(x, y);
     
     coord.rotate(this.rotation);
     coord.scale(this.scale, this.scale);
     coord.translate(this.x, this.y);
+
+    return coord;
+  }
+
+  resolveCoord (el: TopoBlock, x: number = 0, y: number = 0) :Coord {
+    let thatParents = [];
+    let elParents = [];
+    let coord = new Coord(x, y);
+    let that: TopoBlock = this;
+    let _el: TopoBlock = el;
+    let index = -1;
+
+    while (that) {
+      thatParents.unshift(that);
+      that = that.parent;
+    }
+
+    while (_el) {
+      elParents.unshift(_el);
+      _el = _el.parent;
+    }
+
+    for (let i = thatParents.length - 1; i >= 0; i--) {
+      if ((index = elParents.indexOf(thatParents[i])) !== -1) {
+        break;
+      }
+      coord = thatParents[i].getAbsoluteCoord(coord.x, coord.y);
+    }
+
+    for (let j = index + 1; j < elParents.length; j++) {
+      coord = elParents[j].getRelativeCoord(x, y);
+    }
 
     return coord;
   }
